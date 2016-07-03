e4c1fb6d2876f636cdc93227025e783235a0605e
iboozyvoozy <iboozyvoozy@me.com>
2016-07-03 03:59:25 +0300

    ++

--- config/karma.prod.js
+++ config/karma.prod.js
@@ -3,12 +3,11 @@ const base = require('./karma.base');
 module.exports = config => {
 
 	const patch = Object.assign({}, base, {
-		browsers: ['Chrome', 'Firefox'],
+		browsers: ['Firefox'],
 		autoWatch: false,
 		singleRun: true
 	});
 
-	patch.plugins.push('karma-chrome-launcher');
 	patch.plugins.push('karma-firefox-launcher');
 
 	config.set(patch);
